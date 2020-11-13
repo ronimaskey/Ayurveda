c380be2ebe33cb41a93d28d99ae15eebf2e84f3f
ronimaskey <ronimaskey@gmail.com>
2020-11-13 22:17:17 +0100

    [edited]Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import './App.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
@@ -10,18 +11,24 @@ import Cart from './components/Cart';
 import Default from './components/Default';
 
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar/>
-        <Switch>
-          <Route path="/" exact component={ProductList} />
-          <Route path="/details" component={Details} />
-          <Route path="/cart" component={Cart} />
-          <Route component={Default} />
-        </Switch>
-    </React.Fragment>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <ProductList />
+        <Details />
+        <Cart />
+        <Default />
+      <div className="container"> 
+        <div className="row">
+          <div className="col-6">coloumn one</div>
+          <div className="col-6">coloumn two</div>
+        </div>
+      </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import {ButtonContainer} from './Button';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import logo from "../logo.png";
 
 export default class Navbar extends Component {
   render() {
     return (
-        <div> 
-          navbar
-        </div>
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        <Link to="/">
+          <img src={logo} alt="store" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <ButtonContainer>
+          <span className="mr-2">
+          <span className="mr-2">
+            <FontAwesomeIcon icon={faShoppingCart} /> </span>
+            Add to cart
+            </span>
+          </ButtonContainer>
+        </Link>
+      </nav>
     );
   }
 }
